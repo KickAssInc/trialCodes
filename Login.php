5de00ae93da1500ebe6ddcfd405d5585d0febf28
Akshay Gadhave <akshaygadhave531@gmail.com>
2019-11-18 23:56:21 +0530

    PHP and HTML codes for login authentication
    Check it out...

---
+++ Login.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ToDo";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+	 $username = $_POST['uname'];
+     $password = $_POST['pass'];
+     
+     $sql = "SELECT * FROM signuptable WHERE username='" . $username . "'";
+
+	$result = $conn->query($sql);
+	$count  = mysqli_num_rows($result);
+	if($count==0) {
+		echo "User not found!";
+	} else {
+		$row = $result->fetch_assoc();
+		if($row["password"]==$password)		
+		echo "You are successfully authenticated!";
+		else
+			echo "Invalid Password!";
+		
+	}
+
+$conn->close();
+?>
