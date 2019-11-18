8bfb228e33f90dd489bc35bca7d4cfd15b3d2131
Akshay Gadhave <akshaygadhave531@gmail.com>
2019-11-19 00:03:16 +0530

    SQL Database file

--- todo.sql
+++ todo.sql
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `signuptable`
+-- table structure for table `signuptable`
 --
 
 CREATE TABLE `signuptable` (
